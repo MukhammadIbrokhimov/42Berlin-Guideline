1eabeb04cfde98575b1bbdc68571ed1c1884b2d6
Mukhammad Ibrokhimov <121773164+mukhammadsiddiq@users.noreply.github.com>
2025-01-05 19:47:49 +0100

    Add files via upload

---
+++ FDF/include/fdf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mukibrok <mukibrok@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/30 19:57:46 by mukibrok          #+#    #+#             */
+/*   Updated: 2025/01/05 18:35:28 by mukibrok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+
+# define FDF_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <math.h>
+# include <limits.h>
+# include "./ft_printf/ft_printf.h"
+# include "./libft/libft.h"
+
+#endif
