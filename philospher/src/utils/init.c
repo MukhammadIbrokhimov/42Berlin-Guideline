bfb79955a6189162d8d07e235848d7c64f651600
Mukhammadsidik Ibrokhimov <mukibrok@c4a8c10.42berlin.de>
2025-06-24 13:56:56 +0200

    Fixing functions according to norm

--- philospher/src/utils/init.c
+++ philospher/src/utils/init.c
@@ -3,20 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mukhammad-ibrokhimov <mukhammad-ibrokhi    +#+  +:+       +#+        */
+/*   By: mukibrok <mukibrok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 14:17:31 by mukibrok          #+#    #+#             */
-/*   Updated: 2025/06/23 16:25:21 by mukhammad-i      ###   ########.fr       */
+/*   Updated: 2025/06/24 13:38:54 by mukibrok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/philosopher.h"
 
-void assign_forks(t_philosopher *philo, t_data *data)
+void	assign_forks(t_philosopher *philo, t_data *data)
 {
-	int left_idx = philo->id - 1;
-	int right_idx = philo->id % data->num_philosophers;
-	
+	int	left_idx;
+	int	right_idx;
+
+	left_idx = philo->id - 1;
+	right_idx = philo->id % data->num_philosophers;
 	if (philo->id % 2 == 1)
 	{
 		philo->left_fork = &data->forks[right_idx];
@@ -29,9 +31,9 @@ void assign_forks(t_philosopher *philo, t_data *data)
 	}
 }
 
-int init_philo(t_data *data)
+int	init_philo(t_data *data)
 {
-	int i;
+	int	i;
 
 	i = -1;
 	while (++i < data->num_philosophers)
@@ -43,49 +45,43 @@ int init_philo(t_data *data)
 		data->philosophers[i].right_fork = &data->forks[(i + 1) % data->num_philosophers];
 		data->philosophers[i].data = data;
 		if (pthread_mutex_init(&data->philosophers[i].meals_eaten_, NULL) != 0)
-        {
-            printf("%sMeals eaten mutex initialization failed for philosopher %d!\n", RED, i + 1);
-            cleanup_partial_philosophers(data, i);
-            return (1);
-        }
-        
-        if (pthread_mutex_init(&data->philosophers[i].last_meal_t, NULL) != 0)
-        {
-            printf("%sLast meal mutex initialization failed for philosopher %d!\n", RED, i + 1);
-            pthread_mutex_destroy(&data->philosophers[i].meals_eaten_);
-            cleanup_partial_philosophers(data, i);
-            return (1);
-        }
+		{
+			printf("%sMeals eaten mutex initialization failed!\n", RED);
+			return (cleanup_partial_philosophers(data, i), 1);
+		}
+		if (pthread_mutex_init(&data->philosophers[i].last_meal_t, NULL) != 0)
+		{
+			printf("%smeal mutex initialization failed!\n", RED);
+			pthread_mutex_destroy(&data->philosophers[i].meals_eaten_);
+			return (cleanup_partial_philosophers(data, i), 1);
+		}
 	}
 	return (0);
 }
 
-int init_data(t_data *data)
+int	init_data(t_data *data)
 {
-    int i;
-    
-    data->philosophers = malloc(sizeof(t_philosopher) * data->num_philosophers);
-    data->forks = malloc(sizeof(pthread_mutex_t) * data->num_philosophers);
-    if (!data->philosophers || !data->forks)
-        return (1);
-    i = -1;
-    while (++i < data->num_philosophers)
-    {
-        if (pthread_mutex_init(&data->forks[i], NULL) != 0)
-        {
-            printf("%sFork mutex initialization failed for fork %d!\n", RED, i);
-            return (cleanup(data), 1);
-        }
-    }
-    if (pthread_mutex_init(&data->print_mutex, NULL) != 0)
-    {
-        printf("%sPrint mutex initialization failed!\n", RED);
-        return (cleanup(data), 1);
-    }
-    if (pthread_mutex_init(&data->death_mutex, NULL) != 0)
-    {
-        printf("%sDeath mutex initialization failed!\n", RED);
-        return (cleanup(data), 1);
-    }
-    return (data->simulation_ended = 0, init_philo(data));
+	int	i;
+
+	data->philosophers = malloc(sizeof(t_philosopher) * data->num_philosophers);
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->num_philosophers);
+	if (!data->philosophers || !data->forks)
+		return (1);
+	i = -1;
+	while (++i < data->num_philosophers)
+	{
+		if (pthread_mutex_init(&data->forks[i], NULL) != 0)
+			return (cleanup(data), printf("%sinitialization failed!\n", RED), 1);
+	}
+	if (pthread_mutex_init(&data->print_mutex, NULL) != 0)
+	{
+		printf("%sPrint mutex initialization failed!\n", RED);
+		return (cleanup(data), 1);
+	}
+	if (pthread_mutex_init(&data->death_mutex, NULL) != 0)
+	{
+		printf("%sDeath mutex initialization failed!\n", RED);
+		return (cleanup(data), 1);
+	}
+	return (data->simulation_ended = 0, init_philo(data));
 }
